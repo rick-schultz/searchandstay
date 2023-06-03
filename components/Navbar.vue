133443c45ff509c0146d6847bfc72e05bc047ab6
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 20:05:58 -0300

    changed navbar links to buttons

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,25 +1,19 @@
 <template>
   <b-navbar toggleable="lg" type="light" class="px-4">
-    <b-navbar-brand href="#">
+    <b-navbar-brand to="/home">
       <b-img class="logo" src="../assets/img/logo.png" fluid alt="Search and Stay Logo" />
     </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse" />
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">
+      <b-navbar-nav class="ml-auto gap-2">
+        <b-button to="/home">
           Home
-        </b-nav-item>
-        <b-nav-item href="#">
-          Link
-        </b-nav-item>
-        <b-nav-item href="#">
-          Link
-        </b-nav-item>
-        <b-nav-item href="#">
-          Link
-        </b-nav-item>
+        </b-button>
+        <b-button variant="primary" to="/new-product">
+          New Product
+        </b-button>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -28,5 +22,8 @@
 <style lang="scss" scoped>
 .logo {
   height: 50px;
+}
+.gap-2 {
+  gap: 0.5rem;
 }
 </style>
