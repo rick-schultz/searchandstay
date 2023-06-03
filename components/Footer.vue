e67aebff61c324aa79bbe207e2b766fa034d0ec4
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 15:35:59 -0300

    created footer and navbar components

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-navbar toggleable="lg" type="light" variant="light" class="p-4">
+    <p class="mx-auto my-0">
+      Copyright © Search&Stay 2020 - 2023 | All rights reserved.
+    </p>
+  </b-navbar>
+</template>
---
+++ components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar toggleable="lg" type="light" class="px-4">
+    <b-navbar-brand href="#">
+      <b-img class="logo" src="../assets/img/logo.png" fluid alt="Search and Stay Logo" />
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse" />
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">
+          Home
+        </b-nav-item>
+        <b-nav-item href="#">
+          Link
+        </b-nav-item>
+        <b-nav-item href="#">
+          Link
+        </b-nav-item>
+        <b-nav-item href="#">
+          Link
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  height: 50px;
+}
+</style>
