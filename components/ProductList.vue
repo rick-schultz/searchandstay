6f3c6d5c1a200e84b536a0da8a7c679bd969b2c9
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 15:37:03 -0300

    created component to display api return

---
+++ components/ProductList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+  id: number
+  name: string
+  active: number
+  order: number
+}
+
+const props = defineProps<Props>()
+
+const { id, name, active, order } = toRefs(props)
+</script>
+
+<template>
+  <ul>
+    <li>{{ id }}</li>
+    <li>{{ name }}</li>
+    <li>{{ active }}</li>
+    <li>{{ order }}</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
