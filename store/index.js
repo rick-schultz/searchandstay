eeae4f97a6e3bffdfd7b828868ad5192b5f493c3
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 20:07:33 -0300

    added logic to store

--- store/index.js
+++ store/index.js
@@ -20,6 +20,51 @@ export const useStore = defineStore('store', {
         }
       })
       this.props = response.data.data.entities
+    },
+    async fetchProp (id) {
+      const response = await axios.get(baseURL + id, {
+        headers: {
+          Authorization: process.env.API_TOKEN
+        }
+      })
+      this.props = Array(response.data.data)
+    },
+    async addProp (name, active) {
+      const newProp = {
+        house_rules: {
+          name,
+          active
+        }
+      }
+      const response = await axios.post(baseURL, newProp, {
+        headers: {
+          Authorization: process.env.API_TOKEN
+        }
+      })
+      this.props = response.data.data.entities
+    },
+    async updateProp (id, name, active) {
+      const newProp = {
+        house_rules: {
+          name,
+          active
+        }
+      }
+      const response = await axios.put(baseURL + id, newProp, {
+        headers: {
+          Authorization: process.env.API_TOKEN
+        }
+      })
+      this.props = response.data.data.entities
+    },
+    async deleteProp (id) {
+      const response = await axios.delete(baseURL + id, {
+        headers: {
+          Authorization: process.env.API_TOKEN
+        }
+      })
+      this.props = response.data.data.entities
+      this.fetchProps()
     }
   }
 })
