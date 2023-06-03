8a3391c72384203c50283a1ffe299b87d59886e4
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 15:37:16 -0300

    added components and info to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
+<script setup lang="ts">
+import { computed, onBeforeMount } from 'vue'
+import { useStore } from '~/store/index'
+
+const store = useStore()
+
+onBeforeMount(() => {
+  store.fetchProps()
+})
+
+interface Prop {
+  id: number
+  name: string
+  active: number
+  order: number
+}
+
+type Props = Prop[]
+
+const props = computed<Props>(() => store.props)
+</script>
+
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <Navbar />
+    <div class="main px-4">
+      <h1>Hello</h1>
+      <ProductList v-for="prop in props" :key="prop.id" v-bind="prop" />
+    </div>
+    <Footer />
+  </div>
 </template>
 
-<script setup lang="ts">
-const title = 'Search & Stay'
-</script>
+<style lang="scss" scoped>
+.main{
+  min-height: 85vh;
+}
+</style>
