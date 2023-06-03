4a3dc1fd2a87581fe9a9d8744a11efde34b94598
Rick Schultz <rick-schultz@hotmail.com>
2023-06-03 20:06:50 -0300

    created new product page

---
+++ pages/new-product.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main px-4">
+    <b-row>
+      <ProductList />
+    </b-row>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main{
+  min-height: 85vh;
+}
+</style>
